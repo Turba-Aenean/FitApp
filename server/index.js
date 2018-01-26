d75bf007d990e106a5b14eafc971a4a020789a67
Ethan Lipkind <Ethan.lipkind@gmail.com>
2018-01-25 20:11:20 -0500

    start setting up graphQL

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const db = require('../database/schema.js');
-const models = require('../database/models.js')
-
+const body_parser = require('body-parser');
+const collections = require('../database/collections.js'); // db functions live in here
 const app = express();
 const port = 4000;
+const graphQLSchema = require('./graphQLSchema.js');
 
-// new models.User({username: 'ethan', password: 'password', profile: 'testing'}).save().then((model) => {
-// 	console.log('saved: ', model);
+// app.use('/graphql', body_parser.json(), graphqlExpress({schema: graphQLSchema}));
+
+// app.post('/users', (req, res) => {
+// 	// use params in axios requests to pass in data.
+// 	collections.usernameTaken(req.query.username, (bool) => {
+// 		if (bool) {
+// 			res.send('username taken.');
+// 		} else {
+// 			collections.createUser(req.query).then((response) => {
+// 				res.status(201).send(response)
+// 			});
+// 		}
+// 	});
 // });
+
 
 app.listen(port, () => console.log('Listening on port', port));
